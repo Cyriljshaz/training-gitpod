e656da20ae29d73a958ff82ae759f412a30f7bcb
Germán Delbianco <german@nomadic-labs.com>
2021-06-29 07:56:00 +0000

    DONOTMERGE: personalize config.sh

--- config.sh
+++ config.sh
@@ -6,8 +6,8 @@ export TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER=yes
 # https://faucet.tzalpha.net/
 # and upload to the working environment
 # set up the path for the wallet and a nickname for the wallet
-gp env -e WALLET=
-gp env -e ALIAS=
+gp env -e WALLET=/workspace/training-gitpod/tz1YTFbu8PwopWmt2mMwksQb3EPm2LmfzuvB.json
+gp env -e ALIAS=german
 # activate the account - re-activate if gitpod needs to refresh
 tezos-client activate account $ALIAS with $WALLET
 # reactivation gives an error, we should improve this with some env trick
