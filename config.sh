9083b8c176c505b8e16987bb72fd94e8158e46eb
Germán Delbianco <german@nomadic-labs.com>
2021-08-16 09:40:48 +0000

    german: add setup [do not merge]
    - Add hangzhounet faucets and env

--- config.sh
+++ config.sh
@@ -1,4 +1,6 @@
 export TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER=yes
-tezos-client --endpoint https://granadanet.api.tez.ie config update
-export COCO_ADDRESS=tz1PARwN2EZW2i19xpWVmZK7viT83dq96dfd
-export BOB_ADDRESS=tz1LsGys5zAgomRYxoTcsa7RWKfECVjPpSmp
+tezos-client --endpoint https://hangzhounet.api.tez.ie config update
+#tezos-client activate account alice with [path of the json faucet file]
+# activation just in case
+tezos-client activate account german with /workspace/training-gitpod/tz1TDXyvTQPyaxb7unwiPEtFJPVLYXHV3cra.json --force
+tezos-client activate account german2 with /workspace/training-gitpod/tz1YEkaaE2jY9H6XDnSSTVS4KPWDo3LAYZSP.json --force
