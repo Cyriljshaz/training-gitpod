85cea020849837191d50863c93a6a0ae3bd75d81
guillaumeduhamel <guillaume.duhamel@edukera.com>
2021-09-08 14:43:36 +0000

    up

--- examples/add_number/add_number_arl.js
+++ examples/add_number/add_number_arl.js
@@ -1,20 +1,12 @@
 const assert = require('assert');
-const { deploy, setQuiet } = require('@completium/completium-cli');
+const { deploy } = require('@completium/completium-cli');
 
-setQuiet(true);
+const test = async () => {
+  const [add_number, _] = await deploy('add_number.arl');
+  await add_number.increment({ arg: { quantity: 2 } });
+  const storage = await add_number.getStorage();
+  const count = storage.toNumber();
+  assert(count == 4, "Invalid counter value");
+}
 
-let add_number;
-
-describe("Test add_number", async () => {
-  it("Deploy contract", async () => {
-    [add_number, _] = await deploy('add_number.arl');
-  });
-  it("Call 'increment' entry", async () => {
-    await add_number.increment({ arg : { quantity : 2 }});
-  });
-  it("Check storage value", async () => {
-    const storage = await add_number.getStorage();
-    const count = storage.toNumber();
-    assert(count == 4, "Invalid counter value");
-  });
-});
+test();
