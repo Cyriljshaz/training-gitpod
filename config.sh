1c51ca605ec9350f8c16e29fed6561b5ec3f6eb9
Mathias Hiron <mathias.hiron@gmail.com>
2022-01-26 20:57:41 +0100

    delete config.sh added by mistake

--- config.sh
+++
@@ -1,4 +0,0 @@
-export TEZOS_CLIENT_UNSAFE_DISABLE_DISCLAIMER=yes
-tezos-client --endpoint https://granadanet.api.tez.ie config update
-export COCO_ADDRESS=tz1PARwN2EZW2i19xpWVmZK7viT83dq96dfd
-export BOB_ADDRESS=tz1LsGys5zAgomRYxoTcsa7RWKfECVjPpSmp
